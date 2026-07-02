8dcd9a250e279e88f238869959162e443c363b37
Chris-Fr-C <christophe.fr.corsi@gmail.com>
2026-07-02 22:06:27 +0200

    colors

--- demo/tui.py
+++ demo/tui.py
@@ -2,7 +2,7 @@
 
 import demo.board as board
 from textual.app import App, ComposeResult
-from textual.widgets import Digits, Placeholder, Footer
+from textual.widgets import Digits, Placeholder, Footer, Label, Button
 from textual.containers import Grid, Container
 from textual.binding import Binding
 
@@ -11,11 +11,48 @@ class BoardAppFooter(Placeholder):
     pass
 
 
+class Cell(Container):
+    """Represents a specific cell of the table"""
+
+    value: board.PowerOfTwo
+    display_str: str
+
+    def __init__(self, value: board.PowerOfTwo, display_str: str, **kwargs):
+        super().__init__(**kwargs)
+        self.value = value
+        self.display_str = display_str
+
+    def compose(self) -> ComposeResult:
+        with Container(classes="Cell"):
+            yield Digits(self.display_str)
+
+    def on_mount(self) -> None:
+        # Adding the dynamic color.
+        if self.value >= board.PowerOfTwo(0):
+            value = min(self.value, board.PowerOfTwo(11))
+            self.add_class(f"val-{value}")
+
+
 class BoardApp(App):
     CSS = """
     Screen { align: center middle;}
     Grid { align: center middle; layout: grid; grid-size: 4 4; height: 95%; padding: 2; }
-    Digits { width: auto; }
+    Digits { width: auto; color: black; }
+    .Cell { border: solid black;  }
+
+    /* Hardcoded values for the colors cause artistiiiiiic */
+    .val-0  { background: #eceff1; color: #607d8b; }
+    .val-1  { background: #e8f5e9; color: #2e7d32; }
+    .val-2  { background: #c8e6c9; color: #1b5e20; }
+    .val-3  { background: #a5d6a7; color: #1b5e20; }
+    .val-4  { background: #e1bee7; color: #4a148c; }
+    .val-5  { background: #ce93d8; color: #4a148c; }
+    .val-6  { background: #ba68c8; color: #ffffff; }
+    .val-7  { background: #f8bbd0; color: #880e4f; }
+    .val-8  { background: #ff8a80; color: #b71c1c; }
+    .val-9  { background: #ff5252; color: #ffffff; }
+    .val-10 { background: #ff1744; color: #ffffff; }
+    .val-11 { background: #d50000; color: #ffffff; }
     """
     GAME = board.Board(board.BoardConfig(placeholder=" "))
     BINDINGS = [
@@ -28,22 +65,27 @@ class BoardApp(App):
             key_display="?",
         ),
         # The ascii codes where annoying to find :'D
-        Binding(key="left", action="left", key_display="↑", description="", show=True),
-        Binding(key="right", action="right", key_display="↓", description="", show=True),
-        Binding(key="up", action="up", key_display="←",description="", show=True),
-        Binding(key="down", action="down",key_display="→", description="", show=True),
+        Binding(key="up", action="up", key_display="↑", description="", show=True),
+        Binding(key="down", action="down", key_display="↓", description="", show=True),
+        Binding(key="left", action="left", key_display="←", description="", show=True),
+        Binding(
+            key="right", action="right", key_display="→", description="", show=True
+        ),
     ]
+
+    def show_grid(self) -> ComposeResult:
+        for row_num, row in enumerate(self.GAME.cells):
+            for col in row:
+                yield Cell(col, self.GAME._display_power(col))
 
     def compose(self) -> ComposeResult:
         with Grid(classes="Board"):
-            for row_num, row in enumerate(self.GAME.cells):
-                for col in row:
-                    yield Placeholder(label=f"{row_num} {col} - ")
+            yield from self.show_grid()
+
         with Container():
             yield Footer()
 
-    def on_ready(self) -> None:
-        # self.set_interval(1, self.update_clock)
+    def on_mount(self) -> None:
         pass
 
 
